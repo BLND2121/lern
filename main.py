820bcb2feadc26a634a800609c2f866eb853f70b
BLND1551 <124783814+BLND2121@users.noreply.github.com>
2024-02-10 03:32:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1174300799707394100
+CHANNEL_ID = 1174300802035228716
 SELF_MUTE = True
-SELF_DEAF = False
+SELF_DEAF = True
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
